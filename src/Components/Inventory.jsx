31c2e971d53a59a62ea89dd3969db15953b18b88
nikhilsingh38 <93506721+nikhilsingh38@users.noreply.github.com>
2022-03-07 10:24:21 +0530

    init 2

--- src/Components/Inventory.jsx
+++ src/Components/Inventory.jsx
@@ -22,40 +22,49 @@ export function Inventory() {
       setInkpens(inkpens + value);
     };
     return (
-      <div className="items">
-        <span>Books:</span>
-        <button className="addBook" onClick={() => handleChange(1)}>
-          +
-        </button>
-        <button className="remBook" onClick={() => handleChange(-1)}>
-          -
-        </button>
-        <span className="totalBooks">{counter}</span>
-        <span>Pens:</span>
-        <button className="addPen" onClick={() => handleChange1(1)}>
-          +
-        </button>
-        <button className="remPen" onClick={() => handleChange1(-1)}>
-          -
-        </button>
-        <span className="totalPens">{pen}</span>
-        <span>Notebooks:</span>
-        <button className="addNotebook" onClick={() => handleChange2(1)}>
-          +
-        </button>
-        <button className="remNotebook" onClick={() => handleChange2(-1)}>
-          -
-        </button>
-        <span className="totalNotebooks">{notebook}</span>
-        <span>InkPens:</span>
-        <button className="addInkpen" onClick={() => handleChange3(1)}>
-          +
-        </button>
-        <button className="remInkpen" onClick={() => handleChange3(-1)}>
-          -
-        </button>
-        <span className="totalInkpens">{inkpens}</span>
+      <>
+        <div className="items">
+          <span>Books:</span>
+          <button className="addBook" onClick={() => handleChange(1)}>
+            +
+          </button>
+          <button className="remBook" onClick={() => handleChange(-1)}>
+            -
+          </button>
+          <span className="totalBooks">{counter}</span>
+        </div>
+        <div className="items">
+          <span>Pens:</span>
+          <button className="addPen" onClick={() => handleChange1(1)}>
+            +
+          </button>
+          <button className="remPen" onClick={() => handleChange1(-1)}>
+            -
+          </button>
+          <span className="totalPens">{pen}</span>
+        </div>
+
+        <div className="items">
+          <span>Notebooks:</span>
+          <button className="addNotebook" onClick={() => handleChange2(1)}>
+            +
+          </button>
+          <button className="remNotebook" onClick={() => handleChange2(-1)}>
+            -
+          </button>
+          <span className="totalNotebooks">{notebook}</span>
+        </div>
+        <div className="items">
+          <span>InkPens:</span>
+          <button className="addInkpen" onClick={() => handleChange3(1)}>
+            +
+          </button>
+          <button className="remInkpen" onClick={() => handleChange3(-1)}>
+            -
+          </button>
+          <span className="totalInkpens">{inkpens}</span>
+        </div>
         <div className="total">{counter + pen + notebook + inkpens}</div>
-      </div>
+      </>
     );
 }
